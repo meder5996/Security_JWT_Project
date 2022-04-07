da7c952f2d7936faf6209d51b85da5482049da8f
AkylbekNurbekov <97018017+AkylbekNurbekov@users.noreply.github.com>
2022-04-07 17:38:44 +0600

    Initial commit

---
+++ src/main/java/peaksoft/SecurityJwtProjectApplication.java
@@ -0,0 +1,13 @@
+package peaksoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityJwtProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityJwtProjectApplication.class, args);
+    }
+
+}
